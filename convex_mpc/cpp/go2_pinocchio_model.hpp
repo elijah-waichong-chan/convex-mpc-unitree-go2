a3abfa37f15ad9d76a85335b98072dd235074c25
Elijah Chan <chanwaichong0352@gmail.com>
2025-12-24 18:40:05 -0800

    pinocchio model

---
+++ convex_mpc/cpp/go2_pinocchio_model.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <Eigen/Dense>
+#include <Eigen/Geometry>
+
+#include <array>
+#include <string>
+#include <vector>
+#include <cstddef>
+
+#include <pinocchio/multibody/model.hpp>
+#include <pinocchio/multibody/data.hpp>
+
+
